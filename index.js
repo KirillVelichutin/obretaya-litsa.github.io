509248bcd368a60ef11b36252ce126e0b0fca56f
Kirill Velichutin <kirillvelicutin0660@gmail.com>
2025-12-27 19:58:15 +0300

    make the list of films to expand and shrink

--- index.js
+++ index.js
@@ -1,38 +1,75 @@
-const mediaQuery = window.matchMedia('(max-width: 550px)');
+const showMore = document.getElementById('show-more');
+const slides = document.getElementsByClassName("member-card");
+const films = document.getElementsByClassName("films-list-item");
 
-function handleScreenChange(e) {
-    if (e.matches) {
+function checkWindowWidth() {
+    if (window.innerWidth <= 550) {
         const prev = document.getElementById('prev');
         const next = document.getElementById('next');
 
         let slideIndex = 1;
         showSlides(slideIndex);
 
-        prev.addEventListener('click', () => changeSlides(-1))
-        next.addEventListener('click', () => changeSlides(1))
+        prev.addEventListener('click', () => changeSlides(-1));
+        next.addEventListener('click', () => changeSlides(1));
 
         function changeSlides(n) {
             showSlides(slideIndex += n);
-        }
+        };
 
         function showSlides(n) {
             let i;
         
-            let slides = document.getElementsByClassName("member-card");
+            if (n > slides.length) {slideIndex = 1};
 
-            if (n > slides.length) {slideIndex = 1}    
-
-            if (n < 1) {slideIndex = slides.length}
+            if (n < 1) {slideIndex = slides.length};
 
             for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";  
-            }
+                slides[i].style.display = "none";
+            };
 
-            slides[slideIndex-1].style.display = "block";  
+            slides[slideIndex-1].style.display = "block";
+        };
+    } else {
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "block";
+        };
+    }
+
+    if (window.innerWidth <= 890) {
+        let filmIndex = 2;
+        
+        for (let n = filmIndex; n < films.length; n++) {
+            films[n].style.display = "none";
+        };
+        
+        showMore.addEventListener('click', () => showFilms(filmIndex));
+    } else {
+        let filmIndex = 3;
+
+        for (let n = filmIndex; n < films.length; n++) {
+            films[n].style.display = "none";
+        };
+
+        showMore.addEventListener('click', () => showFilms(filmIndex));
+    }
+};
+
+function showFilms(filmIndex) {
+    console.log('click')
+
+    for (let k = filmIndex; k < films.length; k++) {
+        if (films[k].style.display === "none") {
+            films[k].style.display = "flex";
+
+            showMore.textContent = 'Скрыть показанные'
+        } else {
+            films[k].style.display = "none";
+
+            showMore.textContent = 'Смотреть ещё'
         }
     };
-}
+};
 
-handleScreenChange(mediaQuery);
-
-mediaQuery.addEventListener('resize', handleScreenChange);
+checkWindowWidth();
+window.addEventListener('resize', checkWindowWidth);
